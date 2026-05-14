150f6e04e74fa21a96db9a1c48a903157ddac071
Pedro Kazumy Lima Watanabe <pedroklw92@gmail.com>
2026-05-14 16:59:08 -0300

    Adicionando funcionaldade ao carrosel

--- home.js
+++ home.js
@@ -1,35 +1,33 @@
-const imagem = document.getElementById("imagem-prototipo")
+const imagem = document.getElementById("imagem-prototipo");
 
 const imagens = [
-    "./images/modoDocumento.png",
-    "./images/modoExpressão1.png",
-    "./images/modoExpressão2.png",
-    "./images/modoExpressão3.png",
-    "./images/modoExpressão4.png",
-    "./images/assistenteDeIA.png",
-    "./images/botãoInformação.png"
-]
+  "./images/modoDocumento.png",
+  "./images/modoExpressão1.png",
+  "./images/modoExpressão2.png",
+  "./images/modoExpressão3.png",
+  "./images/modoExpressão4.png",
+  "./images/assistenteDeIA.png",
+  "./images/botãoInformação.png",
+];
 
-let indice = 0
+let indice = 0;
 
 document.querySelector(".next").addEventListener("click", () => {
+  indice++;
 
-    indice++
+  if (indice >= imagens.length) {
+    indice = 0;
+  }
 
-    if(indice >= imagens.length){
-        indice = 0
-    }
-
-    imagem.src = imagens[indice]
-})
+  imagem.src = imagens[indice];
+});
 
 document.querySelector(".prev").addEventListener("click", () => {
+  indice--;
 
-    indice--
+  if (indice < 0) {
+    indice = imagens.length - 1;
+  }
 
-    if(indice < 0){
-        indice = imagens.length - 1
-    }
-
-    imagem.src = imagens[indice]
-})
+  imagem.src = imagens[indice];
+});
--- script.js
+++ script.js
@@ -2,17 +2,14 @@ let usuarioCorreto = prompt("Digite o seu usuario para logar");
 let senhaCorreta = prompt("Digite sua senha para logar");
 
 function fazerLogin() {
-
   let usuario = document.getElementById("usuario").value;
   let senha = document.getElementById("senha").value;
 
-
   if (usuario === usuarioCorreto && senha === senhaCorreta) {
-    alert("Login realizado com sucesso!")
-    window.open('./home.html');
-    
+    alert("Login realizado com sucesso!");
+    window.open("./home.html");
   } else {
-    alert("Usuário ou senha incorretos!")
+    alert("Usuário ou senha incorretos!");
   }
 }
 
